9eda4a190468dd9095bc0146b7c1f3814b682cee
r4venz <zexceed12300@gmail.com>
2023-09-11 23:07:51 +0700

    views: login: add validation messages

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -7,20 +7,23 @@
     <div class="flex flex-col px-8 w-full md:w-1/5 items-center">
         <span class="font-bold italic mb-6 text-3xl">E-Komite</span>
         <span class="mb-6 text-2xl">Welcome back!</span>
+        @if ($errors->get('password') || $errors->get('email'))
+            <span class="mb-6 text-red-500">Username atau password salah!</span>
+        @endif
         <form class="w-full" method="POST" action="{{ route('login') }}">
             @csrf
             <div class="form-control w-full mb-3">
                 <label class="label">
                     <span class="label-text">Email</span>
                 </label>
-                <input type="text" name="email" placeholder="mail@domain.com"
+                <input required type="text" name="email" placeholder="mail@domain.com"
                     class="input input-bordered rounded-none focus:outline-none w-full" />
             </div>
             <div class="form-control w-full mb-4">
                 <label class="label">
                     <span class="label-text">Password</span>
                 </label>
-                <input type="password" name="password" placeholder="********"
+                <input required type="password" name="password" placeholder="********"
                     class="input input-bordered rounded-none focus:outline-none w-full" />
             </div>
             <div class="form-control mb-8">
